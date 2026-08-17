e20a42029a60a3b133756fd3e310f33f27e43a59
无名 <nocturnemax@163.com>
2026-08-17 13:36:15 +0800

    remove:cf pages相关配置文件

--- next.config.mjs
+++ next.config.mjs
@@ -5,9 +5,6 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-	  images: {
-    unoptimized: true,
-  },
 };
 
 export default withMDX(config);
